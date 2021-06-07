75376419adcfbef3da135e38da4f1ec8a86eed66
Mingfeng Li <lmf950122@gmail.com>
2021-06-07 06:37:00 +0000

    Create more server side rendering

--- express_server.js
+++ express_server.js
@@ -24,16 +24,22 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
+});
  
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls : urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
