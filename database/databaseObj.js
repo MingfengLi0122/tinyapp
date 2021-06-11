3a51acee4fbbb694b237b1943d84c7b87f603787
Mingfeng Li <lmf950122@gmail.com>
2021-06-11 03:41:20 +0000

    Separate database objects as a single file

---
+++ database/databaseObj.js
@@ -0,0 +1,19 @@
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
+
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "user@example.com",
+    password: "$2b$10$AOVC5x0lFdC5BEK6TSb1vOoo3mUI4TflgM5SKa2FNNqcwVefylccu", //original 123123
+  },
+  as344j: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "$2b$10$.2AfTtME1AlqA3ndkQ4AbuvZ0H.bAnCoa7UndQIl2fKlPq/9GvxZy", //original 123321
+  }
+};
+
+module.exports = { urlDatabase, users };
